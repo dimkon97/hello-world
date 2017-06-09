987e1327e261752b4323bd0516339fc229a9dba0
Zeynep Enkavi <zeynepenkavi@gmail.com>
2017-06-09 16:52:21 -0700

    fixed loop

--- utility_theory_and_binary_output_of_choice.R
+++ utility_theory_and_binary_output_of_choice.R
@@ -50,6 +50,27 @@ exp_val_2 <- function(alpha, gamma, gambles){
 #the variables i and j as counters then ex_val(i,j,gambles) should appear somewhere. Any help
 #or insight is welcome. What other graph methods could I use to model this?
 
+# Here are the issues I'm seeing in your code:
+# - Where is this 'heatmap' function coming from? You initialize a plot in ggplot2 using the 'ggplot' function.
+# - What is the intended output of the for loop? As it is the indices (i, j) are not being used for anything and the plotting command is within the loop so you would be making i*j number of plots when you just want one
+# - You currently just have a function to simulate choices given a single value of alpha and a single value of gamma. You need to first figure out how to run this for multiple values of both parameters. It seems that's what you're trying to do in the for loop. To make it work try:
+alphas = seq(0,1,0.1)
+gammas = seq(0,1,0.1)
+counter = 0
+output <- data.frame(alpha=rep(NA, 121), gamma=rep(NA,121), percent_safe=rep(NA,121))
+for (i in 1:length(alphas)){
+  for(j in 1:length(gammas)){
+    counter = counter+1
+    alpha = alphas[i]
+    gamma = gammas[j]
+    output$alpha[counter] = alpha
+    output$gamma[counter] = gamma
+    output$percent_safe[counter] = unique(exp_val_2(alpha, gamma, gambles)$percent)
+  }
+}
 
+library(ggplot2)
 
+ggplot(output, aes(alpha, gamma))+
+  geom_raster(aes(fill=percent_safe))
 

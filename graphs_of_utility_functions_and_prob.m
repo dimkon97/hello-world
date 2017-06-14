277061ed8a7300b724ae39d868057e4bf2be3711
dimkon97 <dkonstan@mit.edu>
2017-06-13 17:25:33 -0700

    Create graphs_of_utility_functions_and_prob.m

--- graphs_of_utility_functions_and_prob.m
+++ graphs_of_utility_functions_and_prob.m
@@ -1,8 +1,13 @@
+#assume p1 = 0.1 and p2 = 0.9
+# I am trying to find the average value of P, to give as an idea if the threshold is actually 0.5 or depends on the choices of
+#p1,p2,alpha,gamma,payoff1,payoff2
+
+
 [alpha,gamma] = meshgrid(0:0.01:2);
 
-U_X1 = real((exp(-(log(0.4)).^gamma)*(100.^alpha) + (1-exp(-(log(0.6)).^gamma))*(80.^alpha)));
+U_X1 = real((exp(-(log(0.1)).^gamma)*(100.^alpha) + (1-exp(-(log(0.9)).^gamma))*(80.^alpha)));
 
-U_Y1 = real((exp(-(log(0.4)).^gamma)*(190.^alpha) + (1-exp(-(log(0.6)).^gamma))*(5.^alpha)));
+U_Y1 = real((exp(-(log(0.1)).^gamma)*(190.^alpha) + (1-exp(-(log(0.9)).^gamma))*(5.^alpha)));
 
 P = (exp(U_X1)./(exp(U_X1) + exp(U_Y1)));
 
